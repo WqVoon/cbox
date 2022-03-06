cbfb71280c628fdbf123a0eb3db51b2896b6bdbd
WqVoon <2027195958@qq.com>
2022-03-06 23:39:28 +0800

    feat: 解析 `rootDir` 参数，并创建其对应的目录作为根目录

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"os"
+	"path"
+)
+
+var (
+	rootDir = flag.String("root_dir", "", "cbox root directory (default $HOME/cbox-dir)")
+)
 
 func main() {
-	fmt.Println("Hello cbox!")
+	flag.Parse()
+
+	log.Println("Hello cbox!")
+
+	prepareRootDir(rootDir)
+	log.Println("root dir:", *rootDir)
+
+	initRootDir(rootDir)
+	log.Println("create root dir successfully :-)")
+}
+
+func prepareRootDir(rootDir *string) {
+	if rootDir == nil || *rootDir == "" {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatalln("faild to get user home dir, err:", err)
+		}
+
+		*rootDir = path.Join(homeDir, "cbox-dir")
+	}
+}
+
+func initRootDir(rootDir *string) {
+	if rootDir == nil {
+		log.Fatalln("`rootDir` can not be nil")
+	}
+
+	path := *rootDir
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		if err = os.MkdirAll(path, 0755); err != nil {
+			log.Fatalln("faild to create directory, err:", err)
+		}
+	}
 }
