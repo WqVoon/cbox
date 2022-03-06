a4b186e1883c2761c8e2e2be0ff1a50c57a5b86d
WqVoon <2027195958@qq.com>
2022-03-07 00:00:31 +0800

    feat: 创建 rootDir 下的 containers 和 images 目录

--- main.go
+++ main.go
@@ -16,14 +16,11 @@ func main() {
 
 	log.Println("Hello cbox!")
 
-	prepareRootDir(rootDir)
-	log.Println("root dir:", *rootDir)
-
 	initRootDir(rootDir)
-	log.Println("create root dir successfully :-)")
+	log.Println("successfully create root dir:", *rootDir)
 }
 
-func prepareRootDir(rootDir *string) {
+func initRootDir(rootDir *string) {
 	if rootDir == nil || *rootDir == "" {
 		homeDir, err := os.UserHomeDir()
 		if err != nil {
@@ -32,17 +29,17 @@ func prepareRootDir(rootDir *string) {
 
 		*rootDir = path.Join(homeDir, "cbox-dir")
 	}
-}
 
-func initRootDir(rootDir *string) {
-	if rootDir == nil {
-		log.Fatalln("`rootDir` can not be nil")
-	}
+	rootPath := *rootDir
+	subPaths := []string{"containers", "images"}
 
-	path := *rootDir
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		if err = os.MkdirAll(path, 0755); err != nil {
-			log.Fatalln("faild to create directory, err:", err)
+	for _, subPath := range subPaths {
+		path := path.Join(rootPath, subPath)
+
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			if err = os.MkdirAll(path, 0755); err != nil {
+				log.Fatalln("faild to create directory, err:", err)
+			}
 		}
 	}
 }
