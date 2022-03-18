91dd154d7a72b9fa38bd0398272e93abd54d0eda
WqVoon <2027195958@qq.com>
2022-03-18 17:32:50 +0800

    feat: runtime 中去掉 mount namespace

--- pkg/runtime/cmd/run.go
+++ pkg/runtime/cmd/run.go
@@ -19,9 +19,10 @@ func Run(containerID string, name string, args []string) {
 		Stdout: os.Stdout,
 		Stderr: os.Stderr,
 
+		// 由于 golang 环境对 mount namespace 使用 setns 会报错，所以先不创建新的 ns
+		// 依靠 chroot 来做隔离，宿主机依然可见容器内的挂载操作
 		SysProcAttr: &unix.SysProcAttr{
 			Cloneflags: unix.CLONE_NEWPID |
-				unix.CLONE_NEWNS |
 				unix.CLONE_NEWUTS |
 				unix.CLONE_NEWIPC,
 		},
