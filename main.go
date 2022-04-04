00f4a82f9a594df30d7dfe636d833feabde430e3
WqVoon <2027195958@qq.com>
2022-04-05 05:17:24 +0800

    refactor: 调整 RegisterCmd 参数，将注册动作转移到 cmd.go

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"flag"
-	"time"
 
 	"github.com/wqvoon/cbox/pkg/cmd"
-	"github.com/wqvoon/cbox/pkg/container"
 	"github.com/wqvoon/cbox/pkg/flags"
-	"github.com/wqvoon/cbox/pkg/image"
 	"github.com/wqvoon/cbox/pkg/rootdir"
 	"github.com/wqvoon/cbox/pkg/runtime"
 	runtimeUtils "github.com/wqvoon/cbox/pkg/runtime/utils"
-	"github.com/wqvoon/cbox/pkg/utils"
 )
 
 func main() {
@@ -20,75 +16,6 @@ func main() {
 
 	if runtimeUtils.IsRuntimeMode() {
 		runtime.Handle()
-	}
-
-	{
-		cmd.RegisterCmd("test", func(args []string) {
-			c := container.CreateContainer(
-				image.GetImage(utils.GetNameTag("alpine")), "test",
-			)
-			c.Start()
-			// TODO: 这里先等1秒，后面整个更优雅的做法
-			time.Sleep(1 * time.Second)
-			c.Exec(args[0:]...)
-		}, "启动，运行一条龙服务（for test）")
-
-		cmd.RegisterCmd("done", func(args []string) {
-			c := container.GetContainerByName("test")
-			c.Stop()
-			c.Delete()
-		}, "停止，删除一条龙服务（for test）")
-
-		cmd.RegisterCmd("run", func(args []string) {
-			imageNameTag, containerName := args[0], args[1]
-			c := container.CreateContainer(
-				image.GetImage(utils.GetNameTag(imageNameTag)), containerName,
-			)
-			c.Start()
-			time.Sleep(1 * time.Second)
-			c.Exec(args[2:]...)
-		}, "create + start，默认运行 entrypoint，命令格式 `cbox run <IMAGE> <CONTAINER> [...args]`")
-
-		cmd.RegisterCmd("create", func(args []string) {
-			imageNameTag, containerName := args[0], args[1]
-			container.CreateContainer(
-				image.GetImage(utils.GetNameTag(imageNameTag)), containerName,
-			)
-		}, "创建容器，命令格式 `cbox create <IMAGE> <CONTAINER>`")
-
-		cmd.RegisterCmd("start", func(args []string) {
-			container.StartContainersByName(args...)
-		}, "启动容器，命令格式 `cbox start <CONTAINER>`")
-
-		cmd.RegisterCmd("exec", func(args []string) {
-			name := args[0]
-			c := container.GetContainerByName(name)
-			c.Exec(args[1:]...)
-		}, "在容器环境下执行命令, 默认运行 entrypoint，命令格式 `cbox exec <CONTAINER> [...args]`")
-
-		cmd.RegisterCmd("stop", func(args []string) {
-			container.StopContainersByName(args...)
-		}, "停止容器，命令格式 `cbox stop <CONTAINER> [...<CONTAINER>]`")
-
-		cmd.RegisterCmd("rm", func(args []string) {
-			container.DeleteContainersByName(args...)
-		}, "删除容器，命令格式 `cbox rm <CONTAINER> [...<CONTAINER>]`")
-
-		cmd.RegisterCmd("ps", func(args []string) {
-			container.ListAllContainer()
-		}, "列出所有的容器，命令格式 `cbox ps`")
-
-		cmd.RegisterCmd("pull", func(args []string) {
-			image.Pull(utils.GetNameTag(args[0]))
-		}, "拉取镜像到本地，命令格式 `cbox pull <CONTAINER>`")
-
-		cmd.RegisterCmd("images", func(args []string) {
-			image.ListAllImage()
-		}, "列出本地所有镜像，命令格式 `cbox images`")
-
-		cmd.RegisterCmd("rmi", func(args []string) {
-			image.DeleteImagesNyName(args...)
-		}, "删除本地的镜像，命令格式 `cbox rmi <IMAGE> [...<IMAGE>]`")
 	}
 
 	args := flag.Args()
--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"time"
+
+	"github.com/wqvoon/cbox/pkg/container"
+	"github.com/wqvoon/cbox/pkg/image"
 	"github.com/wqvoon/cbox/pkg/log"
+	"github.com/wqvoon/cbox/pkg/utils"
 )
 
 type Cmd struct {
@@ -17,7 +22,7 @@ type CmdSet struct {
 // 保存所有被注册的命令
 var cmdSet CmdSet
 
-func RegisterCmd(name string, fn func(args []string), description string) {
+func RegisterCmd(name, description string, fn func(args []string)) {
 	if cmdSet.name2Fn == nil {
 		cmdSet.name2Fn = make(map[string]func(args []string))
 	}
@@ -30,14 +35,120 @@ func Run(cmdName string, args []string) {
 	if fn, isIn := cmdSet.name2Fn[cmdName]; isIn {
 		fn(args)
 	} else {
-		log.Println("unsupported cmd:", cmdName)
+		log.Errorln("unsupported cmd:", cmdName)
 	}
 }
 
 func init() {
-	RegisterCmd("help", func([]string) {
-		for _, cmd := range cmdSet.cmds {
-			log.Println(cmd.name, "\t", cmd.description)
-		}
-	}, "显示帮助信息")
+	RegisterCmd(
+		"help",
+		"显示帮助信息",
+		func([]string) {
+			for _, cmd := range cmdSet.cmds {
+				log.Println(cmd.name, "\t", cmd.description)
+			}
+		})
+
+	RegisterCmd(
+		"test",
+		"启动，运行一条龙服务（for test）",
+		func(args []string) {
+			c := container.CreateContainer(
+				image.GetImage(utils.GetNameTag("alpine")), "test",
+			)
+			c.Start()
+			// TODO: 这里先等1秒，后面整个更优雅的做法
+			time.Sleep(1 * time.Second)
+			c.Exec(args[0:]...)
+		})
+
+	RegisterCmd(
+		"done",
+		"停止，删除一条龙服务（for test）",
+		func(args []string) {
+			c := container.GetContainerByName("test")
+			c.Stop()
+			c.Delete()
+		})
+
+	RegisterCmd(
+		"run",
+		"create + start，默认运行 entrypoint，命令格式 `cbox run <IMAGE> <CONTAINER> [...args]`",
+		func(args []string) {
+			imageNameTag, containerName := args[0], args[1]
+			c := container.CreateContainer(
+				image.GetImage(utils.GetNameTag(imageNameTag)), containerName,
+			)
+			c.Start()
+			time.Sleep(1 * time.Second)
+			c.Exec(args[2:]...)
+		})
+
+	RegisterCmd(
+		"create",
+		"创建容器，命令格式 `cbox create <IMAGE> <CONTAINER>`",
+		func(args []string) {
+			imageNameTag, containerName := args[0], args[1]
+			container.CreateContainer(
+				image.GetImage(utils.GetNameTag(imageNameTag)), containerName,
+			)
+		})
+
+	RegisterCmd(
+		"start",
+		"启动容器，命令格式 `cbox start <CONTAINER>`",
+		func(args []string) {
+			container.StartContainersByName(args...)
+		})
+
+	RegisterCmd(
+		"exec",
+		"在容器环境下执行命令, 默认运行 entrypoint，命令格式 `cbox exec <CONTAINER> [...args]`",
+		func(args []string) {
+			name := args[0]
+			c := container.GetContainerByName(name)
+			c.Exec(args[1:]...)
+		})
+
+	RegisterCmd(
+		"stop",
+		"停止容器，命令格式 `cbox stop <CONTAINER> [...<CONTAINER>]`",
+		func(args []string) {
+			container.StopContainersByName(args...)
+		})
+
+	RegisterCmd(
+		"rm",
+		"删除容器，命令格式 `cbox rm <CONTAINER> [...<CONTAINER>]`",
+		func(args []string) {
+			container.DeleteContainersByName(args...)
+		})
+
+	RegisterCmd(
+		"ps",
+		"列出所有的容器，命令格式 `cbox ps`",
+		func(args []string) {
+			container.ListAllContainer()
+		})
+
+	RegisterCmd(
+		"pull",
+		"拉取镜像到本地，命令格式 `cbox pull <CONTAINER>`",
+		func(args []string) {
+			image.Pull(utils.GetNameTag(args[0]))
+		})
+
+	RegisterCmd(
+		"images",
+		"列出本地所有镜像，命令格式 `cbox images`",
+		func(args []string) {
+			image.ListAllImage()
+		})
+
+	RegisterCmd(
+		"rmi",
+		"删除本地的镜像，命令格式 `cbox rmi <IMAGE> [...<IMAGE>]`",
+		func(args []string) {
+			image.DeleteImagesNyName(args...)
+		})
 }
